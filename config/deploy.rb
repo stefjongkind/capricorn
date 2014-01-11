ae86c0e978ff1a9673f5a30b6ed1b3ed25fa36ef
Stef Jongkind <stefjongkind@gmail.com>
2014-01-12 11:22:39 +1300

    layout 1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,18 @@
-set :user, 'waiatear'
-set :svn_username, "waiatear" 
-set :server, 'waiateara.co.nz'
-set :svnserver, 'waiateara.svnrepository.com'
-set :application, 'capricorn'
-set :applicationdir, 'railsapp/capricorn' 
-set :repository, "http://#{svnserver}/svn/#{application}/" 
-role :web, 'waiateara.co.nz'
-role :app, 'waiateara.co.nz'
-
+set :user, 'waiatea2'
+role :web, 'capricornz.com'
+role :app, 'capricornz.com'
+role :db,  'capricornz.com', :primary => true
+set :applicationdir, "railsapp/capricornz"
+ssh_options[:forward_agent] = true  
+set :repository, "git@github.com:stefjongkind/capricornz.git"  # Your clone URL
+set :scm, "git"
+set :scm_user, "stefjongkind"
+set :scm_password, Proc.new { CLI.password_prompt "SVN Password: "}
+set :branch, "master"
 set :use_sudo, false                       # HostingRails users don\\\'t have sudo access
 set :deploy_to, "/home/#{user}/#{applicationdir}" 
+
 set:keep_releases, 5
+set :group_writable, false
 default_run_options[:pty] = true
-
-
-
+set :deploy_via, :remote_cache
