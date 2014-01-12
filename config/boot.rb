f0bf0cbc05fdeb5abbedd8631d5f5a6da874843d
Stef Jongkind <stefjongkind@gmail.com>
2014-01-12 13:33:48 +1300

    boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,4 @@
+require 'thread'
 # Don't change this file!
 # Configure your app in config/environment.rb and config/environments/*.rb
 
@@ -81,10 +82,10 @@ module Rails
       end
 
       def load_rubygems
+        min_version = '1.3.2'
         require 'rubygems'
-
-        unless rubygems_version >= '0.9.4'
-          $stderr.puts %(Rails requires RubyGems >= 0.9.4 (you have #{rubygems_version}). Please `gem update --system` and try again.)
+        unless rubygems_version >= min_version
+          $stderr.puts %Q(Rails requires RubyGems >= #{min_version} (you have #{rubygems_version}). Please `gem update --system` and try again.)
           exit 1
         end
 
