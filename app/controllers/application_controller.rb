616fe9364080a45eccc0e47e042bbe9d227d5f39
Stef Jongkind <stefjongkind@waiateara.co.nz>
2014-01-14 21:38:37 +1300

    message and menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,11 @@
 class ApplicationController < ActionController::Base
   # Pick a unique cookie name to distinguish our session data from others'
 #  session :session_key => '_capricorn_session_id'
+before_filter :instantiate_controller_and_action_names
+
+def instantiate_controller_and_action_names
+      @current_action = action_name
+      @current_controller = controller_name
+end
+
 end
--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -7,6 +7,25 @@ class PublicController < ApplicationController
   end
   
   def contact
+    @email = params[:email] if params[:email]
+    @message = params[:msg] if params[:msg]
+    @name = params[:name] if params[:name]
+    if request.post?    
+      unless (@email.blank? or @name.blank? or @message.blank?)
+        if emails_are_valid?(@email)
+          if Notification.deliver_enquiry_notification(@email, @name, @message)
+           flash[:notice] = "Thanks, #{@name} for your enquiry. We will get back to you as soon as possible"
+           redirect_to :action => :contact, :anchor => 'errorExplanation'
+          end
+        else
+          flash.now[:notice] = "Given email address doesn't appear to be valid"
+          render :action => :contact, :anchor => 'errorExplanation'
+        end
+      else
+        flash.now[:notice] = "Please, enter details in all fields"
+        render :action => :contact, :anchor => 'errorExplanation'
+      end
+    end
   end
       
   def database
@@ -30,5 +49,11 @@ class PublicController < ApplicationController
   def vb
   end
       
+  private
+
+  def emails_are_valid?(emails)
+       regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+       !!emails.match(regex)
+  end
       
 end
