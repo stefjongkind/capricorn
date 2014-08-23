e0a2201790c99762f7ddb6de023d2a3c14a38e4b
Stef Jongkind <stefjongkind@gmail.com>
2014-08-23 21:42:24 +1200

    notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,8 +3,7 @@ class Notification < ActionMailer::Base
   def enquiry_notification(email, name, message)
     @message = message
     @name = name
-#    recipients 'info@capricornz.com'
-    recipients 'stefjongkind@gmail.com'
+    recipients 'info@capricornz.com'
     from email
     subject "Capricornz enquiry"
     sent_on Time.now
